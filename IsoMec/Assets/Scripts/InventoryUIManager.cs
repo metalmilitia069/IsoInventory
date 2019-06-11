04a3933eb54f71751cc261f194dceeaedc1abe8e
Medina <medina.engineering@outlook.com>
2019-06-10 20:23:55 -0400

    Commit26
    +aiaiaia

--- IsoMec/Assets/Scripts/InventoryUIManager.cs
+++ IsoMec/Assets/Scripts/InventoryUIManager.cs
@@ -39,6 +39,7 @@ public class InventoryUIManager : MonoBehaviour
         //dictionaryOfInventorySlots.OrderBy(x => x.Key);
         listOfinventorySlots.AddRange(FindObjectsOfType<InventorySlot>());
 
+        
        
         InventoryUIManager.instance.listOfinventorySlots = InventoryUIManager.instance.listOfinventorySlots.OrderBy(tile => tile.name).ToList();
 
@@ -99,12 +100,90 @@ public class InventoryUIManager : MonoBehaviour
         int i = 0;
         int j = 0;
         int q = 0;
+        bool done = false;
+
+        //for (int w = 0; w < this.listOfinventorySlots.Count; w++)
+        //{            
+        //    j = 0;
+        //    while (i < item.itemInventorySize.y + l)
+        //    {
+        //        //q = j;
+        //        //if (!locked)
+        //        //{
+        //        //    j = 0;
+        //        //}
+        //        //j = 0;                
+        //        while (j < item.itemInventorySize.x + q)
+        //        {
+        //            k = ((i * 10) + (j * 1));
+        //            if (this.listOfinventorySlots[k].storedItem == null)
+        //            {
+        //                this.groupOfSelectedInventorySlots.Add(this.listOfinventorySlots[k]);
+        //                this.listOfinventorySlots[k].storedItem = item;
+        //                j++;
+        //                locked = false;
+        //                //return;
+        //            }
+        //            else
+        //            {
+        //                locked = true;
+        //                //i = (int)groupOfSelectedInventorySlots.First().cellSlotCoordinates.y;
+        //                //j = (int)groupOfSelectedInventorySlots.First().cellSlotCoordinates.x;
+        //                i = k / 10;
+        //                j = k % 10;
+        //                j++;
+        //                l = i;
+        //                q = j;
+        //                this.groupOfSelectedInventorySlots.Clear();
+
+
+
+        //                if (j > this.maximumCoordenates.x)
+        //                {
+        //                    j = 0;
+        //                    //l = i+ 1;
+        //                }
+        //                if (i > this.maximumCoordenates.y)
+        //                {
+        //                    Debug.Log("inventory is full");
+        //                    break;
+        //                }
+        //                break;
+        //                //i = k / 10;
+        //                //j = k % 10;
+
+        //                //j++;
+        //            }
+        //            //j++;
+        //        }
+        //        if (!locked)
+        //        {
+        //            i++;
+        //            //j = 0;
+        //        }
+
+        //        //l = i;
+        //    }
+        //}
 
         for (int w = 0; w < this.listOfinventorySlots.Count; w++)
         {
-            while (i < item.itemInventorySize.y + l)
+            if (done)
+            {
+                break;
+            }
+            j = w % 10;
+            i = w / 10;
+            q = j;
+            l = i;
+            if (j > this.maximumCoordenates.x)
             {
                 j = 0;
+            }
+
+            while (i < item.itemInventorySize.y + l)
+            {
+                j = w % 10;
                 while (j < item.itemInventorySize.x + q)
                 {
                     k = ((i * 10) + (j * 1));
@@ -113,22 +192,36 @@ public class InventoryUIManager : MonoBehaviour
                         this.groupOfSelectedInventorySlots.Add(this.listOfinventorySlots[k]);
                         this.listOfinventorySlots[k].storedItem = item;
                         j++;
+                        done = true;
+                        //return;
                     }
                     else
-                    {
+                    {                        
                         this.groupOfSelectedInventorySlots.Clear();
-                        j++;
-                        q = j;
+                        done = false;
+                        j = (int)item.itemInventorySize.x + q;
+                        i = (int)item.itemInventorySize.y + l;
+                        //if (j > this.maximumCoordenates.x)
+                        //{
+                        //    j = 0;
+                        //    //l = i+ 1;
+                        //}
+                        if (i > this.maximumCoordenates.y)
+                        {
+                            Debug.Log("inventory is full");
+                            break;
+                        }
+                        break;                       
                     }
                     //j++;
                 }
                 i++;
             }
         }
-        
 
 
-       //break;
+
+        //break;
 
         //}
         this.PutItemIconButtonIntoInventoryUI();
